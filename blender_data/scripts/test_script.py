59e6c00f7f72963b96ff26d922445cb1f9d4e818
andres-fr <fr_andres@msn.com>
2019-03-18 06:55:30 +0100

    added blender-compatible argparse

--- blender_data/scripts/test_script.py
+++ blender_data/scripts/test_script.py
@@ -7,9 +7,7 @@ D = bpy.data
 # https://blender.stackexchange.com/questions/23943/operator-class-retrieval
 # import bpy.ops as O
 
-import makewalk
 
-print(makewalk.bl_info)
 
 # start blender from CLI with this script
 # blender <OPTIONAL_BLENDER_FILE> --python <PYTHON_SCRIPT>
@@ -19,6 +17,11 @@ print(makewalk.bl_info)
 ###############################################################################
 
 # bpy.data important fields: objects, scenes, materials
+# top-level data containers are so called ID data-blocks. These include Scene,
+# Group, Object, Mesh, Screen, World, Armature, Image and Texture, and get
+# IDs are the only types that can be serialized in the .blend file.
+# e.g. run D.meshes.keys() to see the current meshes.
+
 
 # hovering over buttons shows the command they relate to. e.g.
 # bpy.data.objects["Cube"].location[0]
@@ -34,6 +37,11 @@ print(makewalk.bl_info)
 # opposed to global operations, the CONTEXT corresponds to active/visible
 # elements. It is mainly read-only, can only be modified through the API
 # C.scene.objects.active = obj  # instead of bpy.context.object = obj
+# If you are writing a user tool normally you want to use the bpy.context since
+# the user normally expects the tool to operate on what they have selected.
+# For automation you are more likely to use bpy.data since you want to be able
+# to access specific data and manipulate it, no matter what the user currently
+# has the view set at.
 
 # Operators are tools generally accessed by the user from buttons, menu items
 # or key shortcuts. See help-> operator cheat sheet. Many operators are
@@ -118,6 +126,27 @@ print(makewalk.bl_info)
 # libraries, like argparse, sys.exit when invalid arguments. Workaround
 # see https://docs.blender.org/api/blender2.8/info_gotcha.html
 
+
+# Mathutils: support for vectors, quaternion, eulers, matrix and color types.
+# all operations that apply on vectors can also apply on collections like lists
+# tuples etc. Example of matrix*vector multiplication:
+#     bpy.context.object.matrix_world * bpy.context.object.data.verts[0].co
+# the copy() method drops the reference, useful to avoid side effects:
+#    location_cp = bpy.context.object.location.copy()
+
+# Animations: An important concept is "KEYFRAMES". An object has a collection
+# of per-frame states, and interpolates between them. Small sample:
+#    obj = bpy.context.object
+#    obj.location[2] = 0.0
+#    obj.keyframe_insert(data_path="location", frame=10.0, index=2)
+#    obj.location[2] = 1.0
+#    obj.keyframe_insert(data_path="location", frame=20.0, index=2)
+# Each ID block may have its own .animation_data field.
+
+# Extending Blender UI in scripts is discouraged, because the reference to the
+# defined class gets lost. Best practice is to extend via modules (e.g. addons)
+
+
 ###############################################################################
 ### HELPERS
 ###############################################################################
@@ -188,9 +217,146 @@ def delete_selected():
     bpy.ops.object.delete()
 
 
+
+def register_class(op_clss):
+    """
+    Call this once to let Blender know about the given class.
+    Parameter: a class (not an instance) of type bpy.types.
+    Operator, Panel...
+    """
+    bpy.utils.register_class(op_clss)
+
+def unregister_class(op_clss):
+    """
+    Call this once to let Blender forget about the given class.
+    Parameter: a class (not an instance) of type bpy.types.
+    Operator, Panel...
+    """
+    bpy.utils.unregister_class(op_clss)
+
+
+###############################################################################
+### UI
+###############################################################################
+
+class SimpleOperator(bpy.types.Operator):
+    """
+    A simple Blender operator, to be registered with bpy.utils.register_class.
+    The class members with the bl_ prefix are documented in the API reference.
+    """
+    bl_idname = "object.simple_operator"
+    bl_label = "Simple Object Operator"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        for ob in context.scene.objects:
+            print(ob)
+        return {'FINISHED'}
+
+
+class HelloWorldPanel(bpy.types.Panel):
+    """
+    Creates a Panel in the Properties->Object window.
+    The class members with the bl_ prefix are documented in the API reference.
+    """
+    bl_label = "Hello World Panel"
+    bl_idname = "OBJECT_PT_hello"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
+
+    def draw(self, context):
+        """
+        This method is automatically called by Blender, and refreshed upon any
+        changes.
+        """
+        layout = self.layout
+
+        obj = context.object
+        # rows can be simple labels, static or dynamic
+        row = layout.row()
+        row.label(text="Hello world!", icon='WORLD_DATA')
+        row = layout.row()
+        row.label(text="Active object is: " + obj.name)
+        # prompt user to edit a specific property
+        row = layout.row()
+        row.prop(obj, "name")
+        # user can trigger operators too
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add")
+
+
+
+
+
 ###############################################################################
 ### MAIN ROUTINE
 ###############################################################################
+
+
+import argparse
+import sys
+
+class ArgumentParserForBlender(argparse.ArgumentParser):
+    """
+    This class is identical to its superclass, except for the parse_args
+    method (see docstring). It resolves the ambiguity generated when calling
+    Blender from the CLI with a python script, and both Blender and the script
+    have arguments. E.g., the following call will make Blender crash because
+    it will try to process the script's -a and -b flags:
+    >>> blender --python my_script.py -a 1 -b 2
+
+    To bypass this issue this class uses the fact that Blender will ignore all
+    arguments given after a double-dash ('--'). The approach is that all
+    arguments before '--' go to Blender, arguments after go to the script.
+    The following calls work fine:
+    >>> blender --python my_script.py -- -a 1 -b 2
+    >>> blender --python my_script.py --
+    """
+
+    def _get_argv_after_doubledash(self):
+        """
+        Given the sys.argv as a list of strings, this method returns the
+        sublist right after the '--' element (if present, otherwise returns
+        an empty list).
+        """
+        try:
+            idx = sys.argv.index("--")
+            return sys.argv[idx+1:] # the list after '--'
+        except ValueError as e: # '--' not in the list:
+            return []
+
+    # overrides superclass
+    def parse_args(self):
+        """
+        This method is expected to behave identically as in the superclass,
+        except that the sys.argv list will be pre-processed using
+        _get_argv_after_doubledash before. See the docstring of the class for
+        usage examples and details.
+        """
+        return super().parse_args(args=self._get_argv_after_doubledash())
+
+parser = ArgumentParserForBlender()
+
+
+parser.add_argument("-q", "--quack",
+                    action="store_true",
+                    help="Quacks foo times if activated.")
+parser.add_argument("-b", "--bar", type=int, default=10,
+                    help="Number of desired quacks")
+args = parser.parse_args()
+QUACK = args.quack
+BAR = args.bar
+
+if QUACK:
+    print("QUACK "*BAR)
+
+
+
+register_class(HelloWorldPanel)
 
 #d = C.collection.objects
 #cube = d["Cube"]
