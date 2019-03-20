88db132aa42767edf41647926629e7c06c466e03
andres-fr <fr_andres@msn.com>
2019-03-20 06:37:56 +0100

    added to startup script: mhx2, maximize, shading

--- blender_data/scripts/dance_scene.py
+++ blender_data/scripts/dance_scene.py
@@ -101,7 +101,7 @@ def delete_selected():
 
 
 def purge_unused_data(categories=[D.meshes, D.materials, D.textures, D.images,
-                                  D.curves, D.lights, D.cameras]):
+                                  D.curves, D.lights, D.cameras, D.screens]):
     """
     Blender objects point to data. E.g., a lamp points to a given data lamp
     object. Removing the objects doesn't remove the data, which may lead to
@@ -153,6 +153,42 @@ class ArgumentParserForBlender(argparse.ArgumentParser):
         """
         return super().parse_args(args=self._get_argv_after_doubledash())
 
+def set_shading_mode(mode="SOLID", screens=[]):
+    """
+    Performs an action analogous to clicking on the display/shade button of
+    the 3D view. Mode is one of "RENDERED", "MATERIAL", "SOLID", "WIREFRAME".
+    The change is applied to the given collection of bpy.data.screens.
+    If none is given, the function is applied to bpy.context.screen (the
+    active screen) only. E.g. set all screens to rendered mode:
+      set_shading_mode("RENDERED", D.screens)
+    """
+    screens = screens if screens else [C.screen]
+    for s in screens:
+        for spc in s.areas:
+            if spc.type == "VIEW_3D":
+                spc.spaces[0].shading.type = mode
+                break # we expect at most 1 VIEW_3D space
+
+
+def maximize_layout_3d_area():
+    """
+    TODO: this function assumes Layout is the bpy.context.workspace.
+    It does the following:
+    1. If there is an area with the given name:
+       1.1. Minimizes any other maximized window
+       1.2. Maximizes the desired area
+    """
+    screen_name = "Layout"
+    area_name = "VIEW_3D"
+    screen = D.screens[screen_name]
+    for a in screen.areas:
+        if a.type == area_name:
+            # If screen is already in some fullscreen mode, revert it
+            if screen.show_fullscreen:
+                bpy.ops.screen.back_to_previous()
+            # Set area to fullscreen (dict admits "window","screen","area")
+            bpy.ops.screen.screen_full_area({"screen": screen, "area": a})
+            break
 
 ###############################################################################
 ### GLOBALS
@@ -168,6 +204,9 @@ class ArgumentParserForBlender(argparse.ArgumentParser):
 # QUACK = args.quack
 # BAR = args.bar
 
+
+INIT_SHADING_MODE = "RENDERED"
+INIT_3D_MAXIMIZED = False
 
 # In Blender, x points away from the cam, y to the left and z up
 # (right-hand rule). Locations are in meters, rotation in degrees.
@@ -191,13 +230,30 @@ CAM_LIGHT_WATTS = 45.0  # intensity of the bulb in watts
 FLOOR_NAME = "Floor"
 FLOOR_SIZE = 10  # in meters
 FLOOR_METALLIC = 0.0  # metalic aspect, ratio from 0 to 1
-FLOOR_SPECULAR = 0.01  # specular aspect, ratio from 0 to 1
-FLOOR_ROUGHNESS = 0.2  # the higher the more light difussion. From 0 to 1
+FLOOR_SPECULAR = 0.0  # specular aspect, ratio from 0 to 1
+FLOOR_ROUGHNESS = 1.0  # the higher the more light difussion. From 0 to 1
 floor_mat_name = FLOOR_NAME+"Material"
-FLOOR_IMG_ABSPATH = '/home/a9fb1e/github-work/human-renderer/blender_data/assets/marble_chess.jpg' 
+FLOOR_IMG_ABSPATH = '/home/a9fb1e/github-work/human-renderer/blender_data/assets/marble_chess.jpg'
+
+MHX2_ABSPATH = "/home/a9fb1e/github-work/human-renderer/makehuman_data/exported_models/tpose_african.mhx2"
+MHX2_NAME = "TposeAfrican"
+
 ###############################################################################
 ### MAIN ROUTINE
 ###############################################################################
+
+# general settings
+
+
+# set all 3D screens RENDERED mode
+
+
+set_shading_mode(INIT_SHADING_MODE, D.screens)
+
+# set fullscreen
+if INIT_3D_MAXIMIZED:
+    maximize_layout_3d_area()
+
 
 # select and delete all objects
 bpy.ops.object.select_all(action="SELECT")
@@ -248,6 +304,14 @@ link_from = floor_img_node.outputs["Color"]
 link_to =  bsdf_inputs["Base Color"]
 floor_material.node_tree.links.new(link_from, link_to)
 
+
+# import makehuman
+bpy.ops.import_scene.makehuman_mhx2(filepath=MHX2_ABSPATH)
+C.object.name = MHX2_NAME
+C.object.data.name = MHX2_NAME
+### BUGGY? IMPOSSIBLE TO EXIT POSE MODE AFTER? C.object.hide_viewport = True  # hide skeleton from 3d view
+
+
 # print(">>>>>>>>>>>>", floor_material.node_tree.links.keys())
 # try:
 #     img = bpy.data.images.load(path)
@@ -263,10 +327,29 @@ floor_material.node_tree.links.new(link_from, link_to)
 # >>> m.node_tree.nodes['Image Texture'].image
 # >>> m = C.object.data.materials['FloorMaterial']
 
-# TODO: the script should:
+# TODO:
 
-# 4. Add a plane with the chess material
+
+# resize floor UV several clicks have to be done:
+# * select floor in obj mode (why obj??)
+# * switch to UV workspace
+# * select edit navigator
+# * click on synch (why??)
+# * select surface
+# * now it is possible to resize.
+# FIND OUT PROGRAMMATIC WAY FOR THIS
+
+
+
 # 5. Add N humans in Tpose at specified positions
 # 6. Load BVH sequences into humans
 # register_class(HelloWorldPanel)
+# TODO: create own workspace? instead of # remove undesired UI
+
+
+# utest or DELETE every implemented code, used or not
+# things like set_shading_mode or maximize_3d_view are to be tested on every window, every screen, every area etc. (e.g. false everywhere except the active one etc).
+
+
+
 
