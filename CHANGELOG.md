1e5dd5b1efeaa757e488d4af591efd34b99ae9d0
andres-fr <fr_andres@msn.com>
2019-03-26 00:49:55 +0100

    working on custom UI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,32 +24,27 @@ All notable changes to this project will be documented in this file.
 ## TODO:
 
 
-- autodoc fails because no bpy. fix that (try using SPHINXBUILD   = ~/opt/blender-2.80-6fd11a21f5c5-linux-glibc224-x86_64/2.80/python/bin/sphinx-build # sphinx-build)
+- Travis script, from fresh OS to utest, coverage, autodoc. TEST ON AN OPEN REPO! (make dummy blender repo?):
+  1. Download Blender 2.80 into home, and set .bashrc env variable so `blender` command works
+  2. Install Python3-DEV (matching version) and copy the python include into blender. Also install other apt packages
+  3. Install pip for blender and pip install requirements
+  4. Pull the add-on repo (automatic?) and create symlinks on the addon folder, for the addon and utest.
+  5. All should work at this point? MAKE SURE that if a CI script "fails", Blender propagates the exception.
 
-- rewrite docstrings in extended "rich-syntax" style
-
-
-- once this is done, check if doing small changes to the package automatically updates upon blender start-up. If positive, we have a clean workflow.
-
-- Then integrate all existing functionality of the script in a structured way. Check that all works on the CI
-
-- probably a good idea to create a standard template for Blender. See https://github.com/osiriswrecks/blender-addon-template/blob/master/ui.py
+Regarding OPEN REPO probably a good idea to create a standard template for Blender. See https://github.com/osiriswrecks/blender-addon-template/blob/master/ui.py
 also see https://blenderartists.org/t/code-template-for-blender-add-ons/682339
 
+- learn about custom UI
+- insert sound sources in scene
+- Get proper specs for Mali data format, and integrate it into MakeWalk via Python parser (with JSON schema or similar).
 
 
 -----------------------------------------
 
 
-
-- Get proper specs for Mali data format, and integrate it into MakeWalk via Python parser (with JSON schema or similar).
-
-
-Integrated workflow:
-   0. See `blender --help` for some CLI interaction
-   1. Render sequences in a headless way, from terminal. Using GPU as fast as possible (headless OpenGL rendering not possible yet? https://developer.blender.org/T54638)
+* Render sequences in a headless way, from terminal. Using GPU as fast as possible (headless OpenGL rendering not possible yet? https://developer.blender.org/T54638)
    
-* TODO: add details about MH export: include skeleton, hide textures under clothes, low poly eyes, **T-SHAPE** (important), no shoes, units in meters, export as binary.
+* add details about MH export: include skeleton, hide textures under clothes, low poly eyes, **T-SHAPE** (important), no shoes, units in meters, export as binary.
 
 TODO: talk about bvh, the approach of MW, existing issues, and how to retarget.
 
--- mpiea_mmr/__init__.py
+++ mpiea_mmr/__init__.py
@@ -37,16 +37,117 @@ bl_info = {
     # 'wiki_url': "",
     "category": "MPIEA"}
 
+# #############################################################################
+# ## UI
+# #############################################################################
+
+# class ObjectSelectPanel(bpy.types.Panel):
+#     bl_idname = "OBJECT_PT_select"
+#     bl_label = "Select"
+#     bl_space_type = 'PROPERTIES'
+#     bl_region_type = 'WINDOW'
+#     bl_context = "object"
+#     bl_options = {'DEFAULT_CLOSED'}
+
+#     @classmethod
+#     def poll(cls, context):
+#         return (context.object is not None)
+
+#     def draw_header(self, context):
+#         layout = self.layout
+#         obj = context.object
+#         layout.prop(obj, "select", text="")
+
+#     def draw(self, context):
+#         layout = self.layout
+
+#         obj = context.object
+#         row = layout.row()
+#         row.prop(obj, "hide_select")
+#         row.prop(obj, "hide_render")
+
+#         box = layout.box()
+#         box.label("Selection Tools")
+#         box.operator("object.select_all").action = 'TOGGLE'
+#         row = box.row()
+#         row.operator("object.select_all").action = 'INVERT'
+#         row.operator("object.select_random")
+
+# class ObjectCursorArray(bpy.types.Operator):
+#     """
+#     Object Cursor Array
+#     """
+#     bl_idname = "object.cursor_array"
+#     bl_label = "Cursor Array"
+#     bl_options = {'REGISTER', 'UNDO'}
+
+#     total = bpy.props.IntProperty(name="Steps", default=2, min=1, max=100)
+
+#     def execute(self, context):
+#         """
+#         """
+#         scene = context.scene
+#         cursor = scene.cursor_location
+#         obj = scene.objects.active
+
+#         for i in range(self.total):
+#             obj_new = obj.copy()
+#             scene.objects.link(obj_new)
+
+#             factor = i / self.total
+#             obj_new.location = (obj.location * factor) + (cursor * (1.0 - factor))
+
+#         return {'FINISHED'}
+
+
+# def menu_func(self, context):
+#     """
+#     """
+#     self.layout.operator(ObjectCursorArray.bl_idname)
+
+# # store keymaps here to access after registration
+# addon_keymaps = []
+
+
+# def register():
+#     bpy.utils.register_class(ObjectCursorArray)
+#     bpy.types.VIEW3D_MT_object.append(menu_func)
+
+#     # handle the keymap
+#     wm = bpy.context.window_manager
+#     # Note that in background mode (no GUI available), keyconfigs are not available either,
+#     # so we have to check this to avoid nasty errors in background case.
+#     kc = wm.keyconfigs.addon
+#     if kc:
+#         km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
+#         kmi = km.keymap_items.new(ObjectCursorArray.bl_idname, 'SPACE', 'PRESS', ctrl=True, shift=True)
+#         kmi.properties.total = 4
+#         addon_keymaps.append((km, kmi))
+
+# def unregister():
+#     # Note: when unregistering, it's usually good practice to do it in reverse order you registered.
+#     # Can avoid strange issues like keymap still referring to operators already unregistered...
+#     # handle the keymap
+#     for km, kmi in addon_keymaps:
+#         km.keymap_items.remove(kmi)
+#     addon_keymaps.clear()
+
+#     bpy.utils.unregister_class(ObjectCursorArray)
+#     bpy.types.VIEW3D_MT_object.remove(menu_func)
+
+
 
 # #############################################################################
 # ## INITIALIZE/REGISTER
 # #############################################################################
 
-classes = [
-    # MCP_PT_Main,
-    # MCP_PT_Utility,
-    # utils.ErrorOperator
-]
+classes = [ObjectSelectPanel]
+
+# classes = [
+#     # MCP_PT_Main,
+#     # MCP_PT_Utility,
+#     # utils.ErrorOperator
+# ]
 
 
 def register():
@@ -67,8 +168,7 @@ def unregister():
 
 if __name__ == "__main__":
     # This gets executed if calling `blender --python <THIS_FILE>.py`
-    # register()
-    pass
+    register()
 
 
 print("[Add-on loaded]: ", bl_info["name"], "version", VERSION)
